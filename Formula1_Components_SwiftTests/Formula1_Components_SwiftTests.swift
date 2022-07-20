d0789858b74a0405179eacb7227d7e7d061451a2
Maarten Wubs <maarten.wubs@icloud.com>
2022-07-20 22:49:03 +0200

    Project adjustments

--- Formula1_Components_SwiftTests/Formula1_Components_SwiftTests.swift
+++ Formula1_Components_SwiftTests/Formula1_Components_SwiftTests.swift
@@ -7,7 +7,7 @@
 
 import XCTest
 
-final class Formula1_Components_SwiftTests: XCTestCase {
+class Formula1_Components_SwiftTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
